5949da06add4491514fd9b386647cda39c8989b7
MRV <sabudilovskiy@yandex.ru>
2024-04-01 10:29:03 +0000

    -

--- src/views/login/handler.hpp
+++ src/views/login/handler.hpp
@@ -4,8 +4,6 @@
 #include <uopenapi/all.hpp>
 #include <userver/storages/postgres/component.hpp>
 #include <userver/storages/postgres/postgres.hpp>
-#include <codegen/sql.hpp>
-#include <helpers/auth.hpp>
 #include <userver/formats/parse/boost_uuid.hpp>
 #include <userver/formats/serialize/boost_uuid.hpp>
 
@@ -16,27 +14,7 @@ namespace views::login{
         handler(const userver::components::ComponentConfig& cfg,
                 const userver::components::ComponentContext& ctx) : base(cfg, ctx),
                 todo_db(ctx.FindComponent<userver::components::Postgres>("todo_db").GetCluster()){}
-        response handle(views::login::Request req) const override{
-            userver::storages::postgres::TimePointTz now(userver::utils::datetime::Now());
-            userver::storages::postgres::TimePointTz end_ts(userver::utils::datetime::Now() + std::chrono::hours(1));
-            auto token = helpers::GetToken(
-                    todo_db,
-                    req.body.login,
-                    req.body.password,
-                    now,
-                    end_ts);
-            if (token){
-                Response200 response200;
-                response200().body.token = *token;
-                return response200;
-            }
-            else {
-                Response400 resp400;
-                resp400().body.message = "Invalid login or password";
-                resp400().body.error_code = models::ErrorCode::invalid_credentials;
-                return resp400;
-            }
-        }
+        response handle(views::login::Request req) const override;
     private:
         userver::storages::postgres::ClusterPtr todo_db;
     };
